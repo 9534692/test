a99e22f649121fa57884816bb7284a063222399e
dfhy <dfhy@DESKTOP-FKV95QU>
2018-06-29 12:35:38 +0800

    1

---
+++ WebApplication1/Controllers/Test1Controller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class Test1Controller : Controller
+    {
+        // GET: Test1
+        public ActionResult Index()
+        {
+            string a(int i) {
+                return "123";
+            }
+
+            string b = a(1);
+
+            return View();
+        }
+    }
+}
